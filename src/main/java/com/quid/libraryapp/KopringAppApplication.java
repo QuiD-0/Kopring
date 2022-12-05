e7adc9cc9e704ac1d8b5c460c1aa29b9359fda01
quid-x <quid@hanpass.com>
2022-12-05 15:41:47 +0900

    init project

---
+++ src/main/java/com/quid/libraryapp/KopringAppApplication.java
@@ -0,0 +1,13 @@
+package com.quid.libraryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopringAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KopringAppApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/quid/libraryapp/dto/book/request/BookRequest.java
@@ -0,0 +1,11 @@
+package com.quid.libraryapp.dto.book.request;
+
+public class BookRequest {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+}
