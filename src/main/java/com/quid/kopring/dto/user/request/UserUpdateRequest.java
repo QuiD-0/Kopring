c18e6efddfdc725450a004a244079ba62c3f27f0
QuiD-0 <wodnd101@naver.com>
2022-12-07 16:22:03 +0900

    유저 수정 테스트

--- src/main/java/com/quid/kopring/dto/user/request/UserUpdateRequest.java
+++ src/main/java/com/quid/kopring/dto/user/request/UserUpdateRequest.java
@@ -1,5 +1,8 @@
 package com.quid.kopring.dto.user.request;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class UserUpdateRequest {
 
   private long id;
--- src/test/kotlin/com/quid/kopring/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/quid/kopring/service/user/UserServiceTest.kt
@@ -2,24 +2,47 @@ package com.quid.kopring.service.user
 
 import com.quid.kopring.domain.user.UserRepository
 import com.quid.kopring.dto.user.request.UserCreateRequest
+import com.quid.kopring.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import javax.transaction.Transactional
 
 @SpringBootTest
+@Transactional
 class UserServiceTest @Autowired constructor(
     private val userRepository: UserRepository,
-    private val userService: UserService) {
+    private val userService: UserService
+) {
+
+    companion object {
+        const val NAME: String = "test"
+        const val UPDATEDNAME : String = "updated"
+    }
 
     @Test
-    fun saveUser(){
-        val request = UserCreateRequest("test",10)
+    fun saveUser() {
+        val request = UserCreateRequest(NAME, 10)
 
         userService.saveUser(request)
 
-        val user = userRepository.findByName("test").get()
-        assertThat(user.name).isEqualTo("test")
+        val user = userRepository.findByName(NAME).get().also { println("name : ${it.name}") }
+        assertThat(user.name).isEqualTo(NAME)
+    }
+
+    @Test
+    fun updateUser() {
+        val request = UserCreateRequest(NAME, 10)
+        userService.saveUser(request)
+
+        val user = userRepository.findByName(NAME).get()
+
+        val updateRequest = UserUpdateRequest(user.id, UPDATEDNAME)
+        userService.updateUserName(updateRequest)
+
+        val updatedUser = userRepository.findByName(UPDATEDNAME).get()
+        assertThat(updatedUser.name).isEqualTo(UPDATEDNAME)
     }
 
 }
