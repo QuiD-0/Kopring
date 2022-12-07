f9662a8024847451fbf24aa97f7cd6b7ee3d3057
QuiD-0 <wodnd101@naver.com>
2022-12-07 14:52:58 +0900

    클래스 테스트

--- src/test/kotlin/com/quid/kopring/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/quid/kopring/calculator/CalculatorTest.kt
@@ -11,4 +11,25 @@ class CalculatorTest{
         calculator.add(5)
         assertEquals(15, calculator.num)
     }
+
+    @Test
+    fun minus_test(){
+        val calculator = Calculator(10)
+        calculator.minus(5)
+        assertEquals(5, calculator.num)
+    }
+
+    @Test
+    fun multiply_test(){
+        val calculator = Calculator(10)
+        calculator.multiply(5)
+        assertEquals(50, calculator.num)
+    }
+
+    @Test
+    fun divide_test(){
+        val calculator = Calculator(10)
+        calculator.divide(5)
+        assertEquals(2, calculator.num)
+    }
 }
