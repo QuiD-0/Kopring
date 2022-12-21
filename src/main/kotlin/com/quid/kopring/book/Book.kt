1f5213cb7074fe2c09038ec886455b00a8d761cd
QuiD-0 <wodnd101@naver.com>
2022-12-21 15:52:03 +0900

    book @GeneratedValue 추가

--- src/main/kotlin/com/quid/kopring/book/Book.kt
+++ src/main/kotlin/com/quid/kopring/book/Book.kt
@@ -1,12 +1,14 @@
 package com.quid.kopring.book
 
-import com.quid.kopring.book.model.request.BookRequest
 import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 class Book(
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
     val name: String,
 ) {
