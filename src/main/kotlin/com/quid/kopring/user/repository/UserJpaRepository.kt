260633952ffed3006cfa5369c93d0a1ea9772190
QuiD-0 <wodnd101@naver.com>
2022-12-23 10:03:44 +0900

    fetch join

--- src/main/kotlin/com/quid/kopring/user/repository/UserJpaRepository.kt
+++ src/main/kotlin/com/quid/kopring/user/repository/UserJpaRepository.kt
@@ -2,7 +2,11 @@ package com.quid.kopring.user.repository
 
 import com.quid.kopring.user.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserJpaRepository : JpaRepository<User, Long> {
     fun findByName(name: String): User?
+
+    @Query("select distinct u from User u left join fetch u.userLoanHistories")
+    fun findAllWithUserLoanHistories(): List<User>
 }
