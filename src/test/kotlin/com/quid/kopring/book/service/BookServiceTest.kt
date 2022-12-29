572e7ff64972c6c03a8d7811491cc96a64e9562b
QuiD-0 <wodnd101@naver.com>
2022-12-29 09:32:55 +0900

    book loan 테스트 코드 작성

--- src/test/kotlin/com/quid/kopring/book/service/BookServiceTest.kt
+++ src/test/kotlin/com/quid/kopring/book/service/BookServiceTest.kt
@@ -1,23 +1,42 @@
 package com.quid.kopring.book.service
 
 import com.quid.kopring.book.model.request.BookCreateRequest
-import com.quid.kopring.book.model.type.BookType
-import com.quid.kopring.book.model.type.BookType.*
+import com.quid.kopring.book.model.request.BookLoanRequest
+import com.quid.kopring.book.model.type.BookType.SCIENCE
+import com.quid.kopring.user.model.request.UserCreateRequest
+import com.quid.kopring.user.service.UserService
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BookServiceTest @Autowired constructor(
-    private val bookService: BookService
+    private val bookService: BookService,
+    private val userService: UserService,
 ) {
+
+    companion object {
+        val create = BookCreateRequest("test", SCIENCE)
+        val loan = BookLoanRequest("user1", "test")
+        val userCreate = UserCreateRequest("user1", 20)
+    }
 
     @Test
     fun statTest() {
-        BookCreateRequest("test", SCIENCE).let { bookService.saveBook(it) }
+        create.let { bookService.saveBook(it) }
 
         val stat = bookService.getStat()
 
         assert(stat.size == 1)
+    }
+
+    @Test
+    fun loanTest() {
+        create.let { bookService.saveBook(it) }
+        userCreate.let { userService.saveUser(it) }
+
+        bookService.loanBook(loan)
+
+        assert(userService.getUserLoanList("user1").loanList.size == 1)
     }
 }
