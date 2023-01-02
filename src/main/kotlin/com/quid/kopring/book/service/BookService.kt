f04e6c7c43788f00530d93b0ef1a03414de62af6
QuiD-0 <wodnd101@naver.com>
2023-01-02 15:10:14 +0900

    로거 추가

--- src/main/kotlin/com/quid/kopring/book/service/BookService.kt
+++ src/main/kotlin/com/quid/kopring/book/service/BookService.kt
@@ -11,6 +11,7 @@ import com.quid.kopring.user.repository.UserJpaRepository
 import com.quid.kopring.userLoanHistory.repository.UserLoanHistoryJpaRepository
 import com.quid.kopring.userLoanHistory.type.UserLoanStatus
 import com.quid.kopring.util.fail
+import mu.KotlinLogging
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -18,15 +19,19 @@ import org.springframework.transaction.annotation.Transactional
 class BookService(
     private val bookRepository: BookRepository,
     private val userJpaRepository: UserJpaRepository,
-    private val userLoanHistoryRepository: UserLoanHistoryJpaRepository
+    private val userLoanHistoryRepository: UserLoanHistoryJpaRepository,
 ) {
+    private val logger = KotlinLogging.logger {}
+
     @Transactional
     fun saveBook(request: BookCreateRequest) {
+        logger.info { "saveBook request: $request" }
         Book(name = request.name, type = request.type).also { bookRepository.save(it) }
     }
 
     @Transactional
     fun loanBook(request: BookLoanRequest) {
+        logger.info { "loanBook request: $request" }
         val book = bookRepository.findByName(request.bookName) ?: fail("Book not found")
 
         if (userLoanHistoryRepository.findByBookNameAndStatus(
@@ -40,27 +45,32 @@ class BookService(
 
     @Transactional
     fun returnBook(request: BookReturnRequest) {
+        logger.info { "returnBook request: $request" }
         userJpaRepository.findByName(request.userName)?.returnBook(request.bookName)
             ?: fail("User not found")
     }
 
     @Transactional(readOnly = true)
     fun getBooks(): List<Book> {
+        logger.info { "getBooks" }
         return bookRepository.findAll()
     }
 
     @Transactional(readOnly = true)
     fun getCountLoanBook(): Int {
+        logger.info { "getCountLoanBook" }
         return userLoanHistoryRepository.findByStatus(UserLoanStatus.LOANED).size
     }
 
     @Transactional(readOnly = true)
     fun getStat(): List<BookStat> {
+        logger.info { "getStat" }
         return bookRepository.getBookStat()
     }
 
     @Transactional
     fun updateBook(id: Long, request: BookUpdateRequest) {
+        logger.info { "updateBook id: $id, request: $request" }
         bookRepository.findById(id)?.update(request) ?: fail("Book not found")
     }
 }
